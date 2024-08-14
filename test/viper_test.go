76d73c3f5e0bdca998578dba126599879dc5b6a5
aris kusnul widitama <aris@teavaro.com>
2024-08-14 09:17:26 +0700

    add config json and yaml

--- test/viper_test.go
+++ test/viper_test.go
@@ -27,3 +27,19 @@ func TestJSON(t *testing.T) {
 	assert.Equal(t, 3306, config.GetInt("database.port"))
 	assert.Equal(t, true, config.GetBool("database.show_sql"))
 }
+
+func TestYaml(t *testing.T) {
+	config := viper.New()
+	config.SetConfigName("config")
+	config.SetConfigType("yaml")
+	config.AddConfigPath("../")
+
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "goviper", config.GetString("app.name"))
+	assert.Equal(t, "aris", config.GetString("app.author"))
+	assert.Equal(t, "localhost", config.GetString("database.host"))
+	assert.Equal(t, 3306, config.GetInt("database.port"))
+	assert.Equal(t, true, config.GetBool("database.show_sql"))
+}
